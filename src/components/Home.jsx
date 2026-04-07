5ed2a4946a88fc5febaa05da6bcde95c9a105910
Antur <anturde21@gmail.com>
2026-04-08 00:05:38 +0600

    made the navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useCallback } from 'react';
+import '../App.css';
+import Navbar from './Navbar';
+import { resumes } from '../../constants';
+import ResumeCard from './ResumeCard';
+
+const Home = () => {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen">
+      <Navbar/>
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+         {resumes.length>0 &&(
+        <div className='resumes-section'>
+             {resumes.map((resume) =>(
+            <ResumeCard key={resume.id} resume={resume}/>
+        ))}
+        </div>
+      )}
+      </section>
+    </main>
+  );
+};
+
+export default Home;
