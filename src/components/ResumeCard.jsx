cea95ec624d020e0b966067174b52b879785eced
Antur <anturde21@gmail.com>
2026-04-08 23:58:32 +0600

    making the cards

--- src/components/ResumeCard.jsx
+++ src/components/ResumeCard.jsx
@@ -27,7 +27,7 @@ const ResumeCard = ({
           <img
             src={imagePath}
             alt="resume"
-            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            className="w-full h-[350px] max-sm:h-50 object-cover object-top"
           />
         </div>
       </div>
